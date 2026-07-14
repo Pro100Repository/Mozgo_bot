65cf308c29841dcbbbed82f4c286bdb9aa9af4bb
Alex <kiriksasha19@gmail.com>
2026-07-14 13:45:28 +0300

    Button

--- handlers/start.py
+++ handlers/start.py
@@ -11,8 +11,8 @@ def main_menu():
     keyboard = ReplyKeyboardMarkup(
         keyboard=[
             [KeyboardButton(text="📅 Предстоящие игры"),    KeyboardButton(text="🏅 Рейтинг команд")],
-            [KeyboardButton(text="❓ FAQ"),                  KeyboardButton(text="📞 Контакты")],
             [KeyboardButton(text="📖 Правила"),              KeyboardButton(text="🎯 Попробуй свои силы")],
+            [KeyboardButton(text="❓ FAQ"),                  KeyboardButton(text="📞 Контакты")],
             [KeyboardButton(text="🔔 Подписка на игры")],
         ],
         resize_keyboard=True
@@ -24,11 +24,14 @@ def main_menu():
 async def cmd_start(message: Message):
     await message.answer(
         f"👋 Привет, {message.from_user.first_name}!\n\n"
-        "Я бот квиз-сообщества *Мзгб*. Здесь ты найдёшь:\n"
+        "Я бот квиз-сообщества *Ruda Games*. Здесь ты найдёшь:\n"
         "📅 Предстоящие игры\n"
         "🏅 Рейтинг команд\n"
+        "📖 Правила\n"
         "❓ Ответы на частые вопросы\n"
-        "📞 Контакты и соцсети\n\n"
+        "📞 Контакты и соцсети\n"
+        "🎯 Можешь испытать свои силы в тестовых выпоросах\n"
+        "🔔 Подписаться на уведомления о будущих играх в твоем городе\n\n"
         "Выбери что тебя интересует 👇",
         reply_markup=main_menu(),
         parse_mode="Markdown"
